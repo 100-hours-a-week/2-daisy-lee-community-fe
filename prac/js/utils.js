fcddec61f0f42928634912b9ac63af845945c049
daisy.lee <zzeonii0612@gmail.com>
2025-02-18 20:50:28 +0900

    first commit

---
+++ prac/js/utils.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // 다른 HTML 파일을 불러와 삽입
+    fetch("../top.html")
+      .then((response) => {
+        if (!response.ok) throw new Error("HTTP error " + response.status);
+        return response.text();
+      })
+      .then((html) => {
+        document.getElementById("header").innerHTML = html;
+      })
+});
