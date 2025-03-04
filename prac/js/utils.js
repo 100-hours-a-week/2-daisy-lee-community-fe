d9c0e85c4e22f0e88f32598505dfbdf05a40e0ef
daisy.lee <zzeonii0612@gmail.com>
2025-03-04 15:48:10 +0900

    Feat: 게시물 목록 json 및 style 수정

--- prac/js/utils.js
+++ prac/js/utils.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     // 다른 HTML 파일을 불러와 삽입
-    fetch("../top.html")
+    fetch("/prac/html/top.html")
       .then((response) => {
         if (!response.ok) throw new Error("HTTP error " + response.status);
         return response.text();
